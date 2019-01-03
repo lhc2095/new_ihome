b1ea0802f9bd179cc674fec73f8c4874df8359a5
lhc_pycharm <lhc_pycharm@163.com>
2019-01-03 09:10:46 +0800

    第二天开始

--- info/modules/api/house.py
+++ info/modules/api/house.py
@@ -1,17 +1,19 @@
 from . import api
 from flask import  request,jsonify,current_app,session,g
-from info.models import House,Area
+from info.models import House,Area,HouseImage
 from info.utils.response_code import RET
-from info import db
+from info import db, constants
 from info.utils.commons import required_login
+from info.utils.image_storage import storage
 
+#获取我的房源列表
 @api.route('/user/houses')
 @required_login
 def new_house():
 
    user=g.user
    try:
-       house_data=House.query.filter().all()
+       house_data=House.query.filter(House.user_id==user.id).all()
    except Exception as e:
        current_app.logger.error(e)
        return jsonify(errrno=RET.DBERR,errmsg='数据库查询失败')
@@ -20,12 +22,13 @@ def new_house():
    houses=[]
    for house in house_data:
        houses.append(house.to_basic_dict())
-   data={
-        'houses':houses
-    }
+   # data={
+   #      'houses':houses
+   #  }
 
-   return jsonify(errno=RET.OK,errmsg='OK',data=data)
+   return jsonify(errno=RET.OK,errmsg='OK',data=houses)
 
+#获取城区
 @api.route('/areas')
 def areas():
     try:
@@ -41,6 +44,7 @@ def areas():
 
     return jsonify(errno=RET.OK, errmsg='OK', data=areas)
 
+#创建新房源
 @api.route('/houses',methods=['GET','POST'])
 @required_login
 def new_houses():
@@ -64,7 +68,7 @@ def new_houses():
             return jsonify(errno=RET.PARAMERR,errmsg='参数缺失')
 
         try:
-            area_id,room_count,acreage,capacity,min_days,max_days=int(area_id),int(room_count),int(acreage),int(capacity),int(min_days),int(max_days),
+            area_id,room_count,acreage,capacity,min_days,max_days,facility=int(area_id),int(room_count),int(acreage),int(capacity),int(min_days),int(max_days),int(facility)
         except Exception as e:
             current_app.logger.error(e)
             return jsonify(errrno=RET.PARAMERR, errmsg='数据类型错误')
@@ -83,7 +87,7 @@ def new_houses():
         house.min_days=min_days
         house.max_days=max_days
         house.user_id=7
-        # house.facilities=facility
+        house.facilities=facility
         try:
             db.session.add(house)
             db.session.commit()
@@ -103,4 +107,82 @@ def new_houses():
 
         return jsonify(errno=RET.OK,errmsg='OK',house_id=house.to_full_dict())
 
+#获取上传房屋图片
+@api.route('houses/<int:house_id>images',methods=['POST'])
+@required_login
+def vimages(house_id):
+    user=g.user
+    new_house=House()
 
+    houseimage=request.form.get('house_image')
+    #检查是否取出参数
+    if not houseimage:
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不完整')
+   # #查询插入图片的房屋信息
+   #  try:
+   #      new_house_msg = House.query.filter(House.id == house_id).first()
+   #  except Exception as e:
+   #      current_app.logger.errno(e)
+   #      return jsonify(errno=RET.DBERR, errmsg='查询房屋信息失败')
+
+
+
+    try:
+        image_name=houseimage.read()
+    except Exception as e:
+        current_app.logger.errno(e)
+        return jsonify(errno=RET.DATAERR,errmsg='读取图片信息失败')
+    try:
+        house_image_name=storage(image_name)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR,errmsg='上传图片失败')
+
+    #生成图片url
+    image=HouseImage()
+    image.url=house_image_name
+    image.house_id=house_id
+
+    #对新创建的房源的图片url进行添加
+    # new_house_msg.query.filter(House.id == house_id).update({"images":house_image_name})
+
+    try:
+        # db.session.add(new_house)
+        db.session.add(image)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DATAERR,errmsg='保存数据库失败')
+
+    data={
+        "url":constants.QINIU_DOMIN_PREFIX+house_image_name
+    }
+
+    return jsonify(errno=RET.OK,errmsg='OK',data=data)
+
+
+#房屋详情页面
+@api.route('/houses/<int:house_id>')
+@required_login
+def house_msg(house_id):
+    user=g.user
+    try:
+        this_house=House.query.filter(House.id == house_id).first()
+    except Exception as e:
+        current_app.logger.errno(e)
+        return jsonify(errno=RET.DBERR,errmsg='查询数据失败')
+
+    #判断房源是否存在
+    if not this_house:
+        return jsonify(errno=RET.NODATA,errmsg='房屋信息不存在')
+
+    this_house_list=[]
+
+    for house_msg in this_house:
+        this_house_list.append(house_msg.to_full_dict())
+
+    data={
+        "house":this_house_list
+    }
+    return jsonify(errno=RET.OK,errmsg='OK',data=data)
